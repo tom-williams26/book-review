958104398ca4530d0f50778a383bd392134caad4
Tom Williams <tom.williams0593@outlook.com>
2025-01-27 01:45:49 +0000

    Defined a one-to-many relationship for books and reviews. Defined a foreign key for the books table and refreshed migrations.

--- database/migrations/2025_01_26_234052_create_reviews_table.php
+++ database/migrations/2025_01_26_234052_create_reviews_table.php
@@ -14,10 +14,23 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
 
+            $table->unsignedBigInteger('book_id');
+
             $table->text('review');
             $table->unsignedTinyInteger('rating');
 
             $table->timestamps();
+
+            /**
+             * To define a foreign key = foreign('...')
+             * references('...') this specifies what column on the other table this foreign key references.
+             * on('...') this needs to be included to reference the table.
+             * onDelete('...') specifying it to be cascade. When a book is deleted, all relating reviews will be deleted too.
+             *  */
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+
+            // Alternatively, shorter syntax is the following, however line 17 will need to be uncommented/removed for the following to work.
+            // $table->foreignId('book_id')->constrained()->cascadeOnDelete();
         });
     }
 
