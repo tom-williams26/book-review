9096d462a34e6f491a599162d13e090378e2b1d3
Tom Williams <tom.williams0593@outlook.com>
2025-03-08 03:55:31 +0000

    Added a review controller (along with a blade file) that will create and store reviews assoicated to a book and will allow the user to select a rating.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +9,9 @@ Route::get('/', function () {
 });
 
 
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)
+    ->only(['index', 'show']);
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
